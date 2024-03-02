9d03048dce85269e7c274fe1dea3ddf006f7fe7b
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-03-02 22:09:21 +0900

    로그인 구현(1)
    UX/UI

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import Form from '@/components/Signin/Form'
+import { FormValues } from '@/models/signin'
+import { useCallback } from 'react'
 
 export default function SigninPage() {
-  return <div>SigninPage</div>
+  const handleSubmit = useCallback((formValues: FormValues) => {
+    console.log('온전한값', formValues)
+  }, [])
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit} />
+    </div>
+  )
 }
