8e75c31a25df8ef42fff6255dee5193ee22c332b
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-03-19 17:38:55 +0900

    카드 신청 구현 - 약관동의

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,3 +1,72 @@
-export default function Terms() {
-  return <div>Terms</div>
+import { termslist } from '@/constants/apply'
+import { useCallback, useState, MouseEvent } from 'react'
+import Agreement from '../shared/Agreement'
+import FixedBottomButton from '../shared/FixedBottomButton'
+
+export default function Terms({
+  onNext,
+}: {
+  onNext: (terms: string[]) => void
+}) {
+  const [termsAgreemnets, setTermsAgreements] = useState(() => {
+    return termslist.reduce<Record<string, boolean>>(
+      (prev, term) => ({
+        ...prev,
+        [term.id]: false,
+      }),
+      {},
+    )
+  })
+
+  const handleAllAgreement = useCallback(
+    (_: MouseEvent<HTMLElement>, checked: boolean) => {
+      setTermsAgreements((prevTerms) => {
+        // 01,02
+        return Object.keys(prevTerms).reduce(
+          (prev, key) => ({
+            ...prev,
+            [key]: checked,
+          }),
+          {},
+        )
+      })
+    },
+    [],
+  )
+
+  const allListSuccess = Object.values(termsAgreemnets).every(
+    (success) => success,
+  )
+
+  return (
+    <div>
+      <Agreement>
+        <Agreement.Title checked={allListSuccess} onChange={handleAllAgreement}>
+          약관에 모두 동의
+        </Agreement.Title>
+        {termslist.map(({ id, title, link }) => (
+          <Agreement.Description
+            key={id}
+            link={link}
+            checked={termsAgreemnets[id]}
+            onChange={(_, checked) => {
+              setTermsAgreements((prevTerms) => ({
+                ...prevTerms,
+                [id]: checked,
+              }))
+            }}
+          >
+            {title}
+          </Agreement.Description>
+        ))}
+      </Agreement>
+      <FixedBottomButton
+        label="약관동의"
+        disabled={allListSuccess === false}
+        onClick={() => {
+          onNext(Object.keys(termsAgreemnets))
+        }}
+      />
+    </div>
+  )
 }
