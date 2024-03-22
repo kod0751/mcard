8c903a6bb350db2d47f7c45c06a9d151265d6c0e
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-03-22 19:46:31 +0900

    카드 신청 구현 - 기본정보

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -1,3 +1,69 @@
-export default function BasicInfo() {
-  return <div>BasicInfo</div>
+import {
+  creditScoreOption,
+  payDateOption,
+  salaryOption,
+} from '@/constants/apply'
+import { ApplyValues } from '@/models/apply'
+import { ChangeEvent, useCallback, useState } from 'react'
+import FixedBottomButton from '../shared/FixedBottomButton'
+import Select from '../shared/Select'
+
+type InfoValues = Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>
+
+export default function BasicInfo({
+  onNext,
+}: {
+  onNext: (infoValues: InfoValues) => void
+}) {
+  const [infoValues, setInfoValues] = useState({
+    salary: '',
+    creditScore: '',
+    payDate: '',
+  })
+
+  const handleInfoChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    setInfoValues((preValues) => ({
+      ...preValues,
+      [e.target.name]: e.target.value,
+    }))
+  }, [])
+
+  const allInfoSelect = Object.values(infoValues).every((value) => value)
+
+  return (
+    <div>
+      <Select
+        name="salary"
+        label="연소득"
+        options={salaryOption}
+        placeholder={salaryOption[0].label}
+        value={infoValues.salary}
+        onChange={handleInfoChange}
+      />
+      <Select
+        name="creditScore"
+        label="신용점수"
+        options={creditScoreOption}
+        placeholder={creditScoreOption[0].label}
+        value={infoValues.creditScore}
+        onChange={handleInfoChange}
+      />
+      <Select
+        name="payDate"
+        label="결제일"
+        options={payDateOption}
+        placeholder={payDateOption[0].label}
+        value={infoValues.payDate}
+        onChange={handleInfoChange}
+      />
+
+      <FixedBottomButton
+        label="다음"
+        onClick={() => {
+          onNext(infoValues)
+        }}
+        disabled={allInfoSelect === false}
+      />
+    </div>
+  )
 }
