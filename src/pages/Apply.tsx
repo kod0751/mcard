98b8d57e6f11d52845732588efeef3ff2c623f09
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-03-31 20:33:20 +0900

    카드중복발급 체크

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,4 +1,6 @@
+import useAppliedCard from '@/components/apply/hooks/useAppliedCard'
 import usePollApplyStatus from '@/components/apply/hooks/usePollApplyStatus'
+import { useAlertContext } from '@/contexts/AlertContext'
 import useUser from '@/hooks/auth/useUser'
 import { APPLY_STATUS } from '@/models/apply'
 import { updateApplyCard } from '@/remote/apply'
@@ -9,11 +11,37 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 export default function ApplyPage() {
   const navigate = useNavigate()
+  const { open } = useAlertContext()
 
   const [readyToPoll, setReadyToPoll] = useState(false)
 
   const user = useUser()
   const { id } = useParams() as { id: string }
+
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied == null) {
+          return
+        }
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다',
+            onButtonClick: () => {
+              window.history.back()
+            },
+          })
+          return
+        }
+
+        setReadyToPoll(true)
+      },
+      onError: () => {},
+      suspense: true,
+    },
+  })
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -52,6 +80,10 @@ export default function ApplyPage() {
       window.history.back()
     },
   })
+
+  if (data != null && data.status === APPLY_STATUS.COMPLETE) {
+    return null
+  }
 
   if (readyToPoll || 카드신청중) {
     return <div>Loading...</div>
