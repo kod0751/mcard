c2d1fd56b1c45a56d17b3b1ffdc1c6aa99d1f509
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-04-11 20:13:48 +0900

    로딩화면 UI개선

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -10,6 +10,12 @@ import useApplyCardMutaion from '@components/apply/hooks/useApplyCardMutaion'
 import { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
+const STATUS_MESSAGE = {
+  [APPLY_STATUS.READY]: '카드 심사를 준비하고있습니다.',
+  [APPLY_STATUS.PROGRESS]: '카드를 심사중입니다. 잠시만 기다려주세요.',
+  [APPLY_STATUS.COMPLETE]: '카드 신청이 완료되었습니다.',
+}
+
 export default function ApplyPage() {
   const navigate = useNavigate()
   const { open } = useAlertContext()
@@ -44,7 +50,7 @@ export default function ApplyPage() {
     },
   })
 
-  usePollApplyStatus({
+  const { data: status } = usePollApplyStatus({
     onSuccess: async () => {
       await updateApplyCard({
         userId: user?.uid as string,
@@ -87,7 +93,7 @@ export default function ApplyPage() {
   }
 
   if (readyToPoll || loading) {
-    return <FullPageLoader message="카드를 신청중입니다" />
+    return <FullPageLoader message={STATUS_MESSAGE[status ?? 'READY']} />
   }
 
   return <Apply onSubmit={mutate} />
